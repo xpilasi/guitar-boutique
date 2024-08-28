645f6bbe71f703adcb09cf3ab3e848fb04cf08fe
Xavier Pilasi <pilasiha@gmail.com>
2024-08-28 21:55:44 +0200

    fixing the slider dots

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -76,11 +76,11 @@ const Slider = () => {
             ))}
         </div>
         {/* DOTS */}
-        <div className='absolute m-auto left-1/2 bottom-8 flex gap-4'>
+        <div className='absolute m-auto w-full justify-center bottom-8 flex gap-4 '>
         {
             slides.map((slide,index) =>(
                 <div 
-                    className={`  w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${currentSlide === index ? 'scale-110' : ''}`}
+                    className={`  w-3 h-3 rounded-full ring-1 ring-gray-600 bg-white bg-opacity-60 cursor-pointer flex items-center justify-center ${currentSlide === index ? 'scale-110' : ''}`}
                     key={slide.id}
                     onClick={()=>setCurrentSlide(index)}
                     >
